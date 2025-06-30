0357fba4f49ab7daa7b8e5b10a3def03afe5fda9
Adeanak <permanadani63@gmail.com>
2025-06-30 11:17:00 +0700

    Fix WebSocket HMR connection error

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,7 @@ import path from "path";
 export default defineConfig(({ mode }) => ({
   server: {
     host: "0.0.0.0",
-    port: 8080,
-    hmr: {
-      protocol: 'ws',
-      host: 'localhost'
-    }
+    port: 8080
   },
   plugins: [
     react()
